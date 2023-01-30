444d3cb3c3ad7e9a8730a97e1d1cb7232a3c4cef
Devfritz <devfritzcode@gmail.com>
2023-01-30 11:20:56 -0500

    setup error middleware

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,4 +1,5 @@
 const User = require("../model/user/model-user");
+const { tryCatch } = require("../utils/tryCatch");
 
 exports.getUsers = (req, res) => {
   return res.status(200).json({
@@ -19,59 +20,51 @@ exports.getUser = async (req, res) => {
 };
 
 //  Register
-exports.register = async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
+exports.register = tryCatch(async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
 
-    //Check if user exist  //
-    const isExist = await User.findOne({ email });
-    if (isExist) {
-      return res.status(401).json({
-        isSuccess: false,
-        message: "User Already Exist",
-      });
-    }
-
-    const newUser = await User.create({ firstName, lastName, email, password });
-    return res.status(201).json({
-      isSuccess: true,
-      data: newUser,
-    });
-  } catch (error) {
-    console.log(error);
+  //Check if user exist  //
+  const isExist = await User.findOne({ email });
+  if (isExist) {
+    throw new Error("User Already Exist");
   }
-};
+
+  const newUser = await User.create({ firstName, lastName, email, password });
+  return res.status(201).json({
+    isSuccess: true,
+    data: newUser,
+  });
+});
 
 //  Login
-exports.login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+exports.login = tryCatch(async (req, res) => {
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.json({
-        message: "All Fields are required",
-      });
-    }
-
-    const userFound = await User.findOne({ email }).select("+password");
-
-    const isMatchPassword = await userFound.comparePassword(password);
-    if (!userFound || !isMatchPassword) {
-      return res.json({
-        message: "Wrong Login Credentials",
-      });
-    }
-
-    if (userFound) {
-      const token = userFound.generateToken(userFound._id);
-      return res.status(200).json({
-        isSuccess: true,
-        message: "Connected Successfully",
-        data: userFound,
-        token,
-      });
-    }
-  } catch (error) {
-    console.log(error);
+  if (!email || !password) {
+    throw new Error("Please Provide an email or password");
   }
-};
+
+  const userFound = await User.findOne({ email }).select("+password");
+
+  if (!userFound) {
+    return res.json({
+      message: "Wrong Login Credentials",
+    });
+  }
+  const isMatchPassword = await userFound.comparePassword(password);
+  if (!isMatchPassword) {
+    return res.json({
+      message: "Wrong Login Credentials",
+    });
+  }
+
+  if (userFound) {
+    const token = userFound.generateToken(userFound._id);
+    return res.status(200).json({
+      isSuccess: true,
+      message: "Connected Successfully",
+      data: userFound,
+      token,
+    });
+  }
+});
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config({ path: ".env" });
 const express = require("express");
 const connectDB = require("./connect/connectDB");
-
+const globalErrorHandler = require("./middleware/globalErrorHandler");
 const app = express();
 
 //  Connection
@@ -13,5 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 // route users
 app.use("/api/v1", require("./route/user-route.js"));
+
+app.use(globalErrorHandler);
+app.all("*", (req, res, next) => {
+  const err = `Can't find  ${req.originalUrl} on the server`;
+  next(err);
+});
 
 app.listen(PORT, () => console.log(`Server is running`));
